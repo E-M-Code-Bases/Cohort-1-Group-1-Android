63c956df464fca039994b8af29c3954ba11e18d3
Jeffgmw <108535585+Jeffgmw@users.noreply.github.com>
2024-07-05 10:47:34 +0300

    resolved error: seeALL

--- app/src/main/java/com/example/starstream/presentation/adapter/FragmentAdapter.kt
+++ app/src/main/java/com/example/starstream/presentation/adapter/FragmentAdapter.kt
@@ -4,13 +4,21 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.starstream.presentation.ui.favoritemovies.FavoriteMoviesFragment
 import com.example.starstream.presentation.ui.favorites.FavoritesFragment
+import com.example.starstream.presentation.ui.fullscreenimage.FullscreenImageFragment
 import com.example.starstream.presentation.ui.home.HomeFragment
+import com.example.starstream.presentation.ui.moviedetails.MovieDetailsFragment
 import com.example.starstream.presentation.ui.movielists.MovieListsFragment
+import com.example.starstream.presentation.ui.seeall.SeeAllFragment
 import com.example.starstream.util.Constants
 
 class FragmentAdapter(private val fragment: Fragment) : FragmentStateAdapter(fragment.childFragmentManager, fragment.viewLifecycleOwner.lifecycle) {
     private val homeFragments = listOf(MovieListsFragment())
     private val favoritesFragments = listOf(FavoriteMoviesFragment())
+
+    private val movieListsFragment = listOf(MovieListsFragment())
+    private val movieDetailsFragment = listOf(MovieDetailsFragment())
+    private val seeAllFragment = listOf(SeeAllFragment())
+    private val fullscreenImageFragment = listOf(FullscreenImageFragment())
 
     override fun getItemCount(): Int = 1
 
@@ -18,6 +26,12 @@ class FragmentAdapter(private val fragment: Fragment) : FragmentStateAdapter(fra
         return when (fragment) {
             is HomeFragment -> homeFragments[position]
             is FavoritesFragment -> favoritesFragments[position]
+
+            is MovieListsFragment -> movieListsFragment[position]
+            is MovieDetailsFragment -> movieDetailsFragment [position]
+            is SeeAllFragment -> seeAllFragment[position]
+            is FullscreenImageFragment -> fullscreenImageFragment[position]
+
             else -> throw IllegalArgumentException(Constants.ILLEGAL_ARGUMENT_FRAGMENT_TYPE)
         }
     }
